b56d437aebf82a3eb9d57eb9f683205ded9a0af7
Sabha Kandula <sabha.kandula@gmail.com>
2019-07-30 13:24:23 -0400

    Recipe List Commit

---
+++ src/components/Recipe.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class Recipe extends Component {
+  render() {
+    const {} = this.props;
+    return (
+      <div>
+        <h1>From Recipe</h1>
+      </div>
+    );
+  }
+}
--- src/pages/Default.js
+++ src/pages/Default.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
-
+import Header from "../components/Header";
+import { Link } from "react-router-dom";
 export default class Default extends Component {
   render() {
     return (
-      <div>
-        <h4>Error Page</h4>
-      </div>
+      <Header title="404" styleClass="default-hero">
+        <h2 className="text-light text-uppercase">You are in wrong place</h2>
+        <Link to="/" className="text-uppercase btn btn-secondary btn-lg mt-3">
+          Return to home
+        </Link>
+      </Header>
     );
   }
 }
