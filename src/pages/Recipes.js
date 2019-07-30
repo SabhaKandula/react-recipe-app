84a8066032b24e1b4ae04dbff1d710394eece939
Sabha Kandula <sabha.kandula@gmail.com>
2019-07-30 19:52:44 -0400

    Final commit with Dynamic API

--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -3,13 +3,44 @@ import RecipeList from "../components/RecipeList";
 import Search from "../components/Search";
 import { recipeData } from "../data/tempList";
 export default class Recipes extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+    this.getRecipes = this.getRecipes.bind(this);
   }
   state = {
     recipes: recipeData,
-    search: ""
+    search: "",
+    url: `https://www.food2fork.com/api/search?key=${
+      process.env.REACT_APP_API_KEY
+    }`,
+    baseUrl: `https://www.food2fork.com/api/search?key=${
+      process.env.REACT_APP_API_KEY
+    }`,
+    query: "&q=",
+    error: ""
   };
+  async getRecipes() {
+    try {
+      const data = await fetch(this.state.url);
+      const jsonData = await data.json();
+      if (jsonData.recipes.length === 0) {
+        this.setState({
+          error:
+            "sorry but your search didn't return any recipes. Please try again or press search icon for most popular recipes"
+        });
+      } else {
+        this.setState({
+          recipes: jsonData.recipes,
+          error: ""
+        });
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  componentDidMount() {
+    this.getRecipes();
+  }
   handleChange = e => {
     this.setState({
       search: e.target.value
@@ -17,6 +48,14 @@ export default class Recipes extends Component {
   };
   handleSubmit = e => {
     e.preventDefault();
+    const { baseUrl, query, search } = this.state;
+    this.setState(
+      {
+        url: `${baseUrl}${query}${search}`,
+        search: ""
+      },
+      () => this.getRecipes() //Call back after setting state with parameters
+    );
   };
   render() {
     return (
@@ -26,7 +65,19 @@ export default class Recipes extends Component {
           handleChange={this.handleChange}
           handleSubmit={this.handleSubmit}
         />
-        <RecipeList recipes={this.state.recipes} />
+        {this.state.error ? (
+          <section>
+            <div className="row">
+              <div className="col">
+                <h2 className="text-orange text-uppercase text-center mt-5">
+                  {this.state.error}
+                </h2>
+              </div>
+            </div>
+          </section>
+        ) : (
+          <RecipeList recipes={this.state.recipes} />
+        )}
       </>
     );
   }
